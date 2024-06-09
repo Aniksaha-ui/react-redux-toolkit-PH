3e916eefd9b26b829f98f9f4529158b4a5f8c647
anik saha <anik.saha@sslwireless.com>
2024-06-09 11:09:41 +0600

    basic redux intalled

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <p>Hello World</p>
+      <Button>My Button</Button>
+    </>
+  );
+}
+
+export default App;
